9f398e1fde6db412cb14bf1f462505e3475fb0cc
sowmya2923 <pothurajusowmya29gmail.com>
2026-04-18 19:25:52 +0530

    Sowmya

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,18 @@ import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './config/db.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import authRoutes from './routes/authRoutes.js';
 import bookingRoutes from './routes/bookingRoutes.js';
 import roomRoutes from './routes/roomRoutes.js';
+import menuRoutes from './routes/menuRoutes.js';
+import diningRoutes from './routes/diningRoutes.js';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -32,6 +39,11 @@ app.get('/', (req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/rooms', roomRoutes);
 app.use('/api/bookings', bookingRoutes);
+app.use('/api/menu', menuRoutes);
+app.use('/api/dining', diningRoutes);
+
+// Static folder for images
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(notFound);
 app.use(errorHandler);
